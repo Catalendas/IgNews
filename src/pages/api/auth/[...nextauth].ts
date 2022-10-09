e1d7b3817a22149ea30c3f4221d3101228c80a31
MarcosAlexandre <82763928+Catalendas@users.noreply.github.com>
2022-10-09 18:59:39 -0300

    atualizacao webhook

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,90 +1,75 @@
-import {query as q, Query} from 'faunadb'
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import { fauna } from "../../../components/services/fauna"
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../components/services/fauna";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    GitHubProvider({
+      clientId: String(process.env.GITHUB_CLIENT_ID),
+      clientSecret: String(process.env.GITHUB_CLIENT_SECRET),
       authorization: {
         params: {
-          scope: 'read:user',
+          scope: "read:user",
         },
       },
     }),
-    // ...add more providers here
   ],
+  secret: String(process.env.NEXTAUTH_SECRET),
   callbacks: {
-    async session({session}) {
+    async session({ session, token, user }) {
       try {
         const userActiveSubscription = await fauna.query(
           q.Get(
             q.Intersection([
               q.Match(
-                q.Index('subscription_by_user_ref'),
+                q.Index("subscription_by_user_ref"),
                 q.Select(
                   "ref",
                   q.Get(
                     q.Match(
-                      q.Index('user_by_email'),
-                      q.Casefold(session.user.email)
+                      q.Index("user_by_email"),
+                      q.Casefold(String(session?.user?.email))
                     )
                   )
                 )
               ),
-              q.Match(
-                q.Index('subscription_by_status'),
-                "active"   
-              )
+              q.Match(q.Index("subscription_by_status"), "active"),
             ])
           )
-        )
-  
+        );
+
         return {
           ...session,
           activeSubscription: userActiveSubscription,
-        }
+        };
       } catch {
         return {
           ...session,
-          activeSubscription: null
-        }
+          activeSubscription: null,
+        };
       }
     },
-
-    async signIn({ user, account, profile}) {
-      const { email } = user
+    async signIn({ user, account, profile }) {
+      const { email } = user;
 
       try {
-        await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
-              )
-            ),
-            q.Create(
-              q.Collection('users'),
-                {data: {email}}
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
+        if (email) {
+          await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+              ),
+              q.Create(q.Collection("users"), { data: { email } }),
+              q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
             )
-          )
-        )
-        return true
-      } catch {
-        return false
+          );
+        }
+
+        return true;
+      } catch (e) {
+        return false;
       }
-    }
-  }
-})
+    },
+  },
+});
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,93 +1,93 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Readable } from 'stream' 
-import Stripe from "stripe";
+import { Readable } from "stream";
 import { stripe } from "../../components/services/stripe";
-import { saveSubscription } from "./_lib/manageSubscription";
+import Stripe from "stripe";
+import { saveSubscription } from "../api/_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
-    const chunks = []
+  const chunks = [];
 
-    for await (const chunk of readable) {
-        chunks.push(
-            typeof chunk == "string" ? Buffer.from(chunk) : chunk
-        )
-    }
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
 
-    return Buffer.concat(chunks)
+  return Buffer.concat(chunks);
 }
 
 export const config = {
-    api: {
-        bodyParser: false
-    }
-}
+  api: {
+    bodyParser: false,
+  },
+};
 
 const relevantEvents = new Set([
-    'checkout.session.completed',
-    'customer.subscription.created',
-    'customer.subscription.updated',
-    'customer.subscription.deleted',
-])
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-   if(req.method == 'POST') {
-       const buf = await buffer(req)
-       const secret = req.headers['stripe-signature']
+const webhooks = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === "POST") {
+    const buf = await buffer(request);
 
-       let event: Stripe.Event
+    //Busca na propriedade `stripe-signature` do cabeçalho da requisição
+    //O id que o stripe envia quando acessa essa rota via webhook.
+    const secret = request.headers["stripe-signature"] as string;
 
-       try {
-        event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    let event: Stripe.Event;
 
-       } catch (err) {
-        console.log(err)
-        return res.status(400).send(`Webhook error: ${err.message}`)
-       }
-   
-       const {type} = event
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        String(process.env.STRIPE_WEBHOOK_SECRET)
+      );
+    } catch (error) {
+      return response.status(400).send(`Webhook error: ${error}`);
+    }
 
-       if(relevantEvents.has(type)) {
-            try {
-                switch(type){
-                    case 'customer.subscription.created':
-                    case 'customer.subscription.updated':
-                    case 'customer.subscription.deleted':
-                        
+    const { type } = event;
 
-                        const subscription = event.data.object as Stripe.Subscription
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
 
-                        await saveSubscription(
-                            subscription.id,
-                            subscription.customer.toString(),
-                            type == 'customer.subscription.created',
-                        )
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString()
+            );
 
-                        break
+            break;
 
-                    case 'checkout.session.completed':
-                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
 
-                        await saveSubscription(
-                            checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString(),
-                            true
-                        )
+            await saveSubscription(
+              String(checkoutSession.subscription),
+              String(checkoutSession.customer),
+              true
+            );
+            break;
 
-                        break
-                        default:
-                            throw new Error('Unhandled event.')
-                }
-            } catch (err) {
-                console.log(err)
-                return res.status(400).json({ error: 'Webhook handle failed.'})
-            }
-       }
+          default:
+            throw new Error("Unhandled event");
+        }
+      } catch (error) {
+        return response.json({
+          error: "Webhook handler failed",
+        });
+      }
+    }
 
+    response.json({ received: true });
+  } else {
+    response.setHeader("Allow", "POST");
+    response.status(405).end("Method not allowed");
+  }
+};
 
-       res.json({ received: true})
-   } else {
-    res.setHeader('Allow', 'POST')
-    res.status(405).end('Method not allowed')
-   }
-}
+export default webhooks;
